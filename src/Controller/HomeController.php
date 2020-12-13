9cff649baaf5bce749342f9bb51b3cfe169fab06
FrancoBorsani <borsafranco@gmail.com>
2020-12-13 17:26:03 -0300

    Obteniendo la ciudad ingresada en js

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -18,4 +18,47 @@ class HomeController extends AbstractController
 
 
 
+
+    /**
+     * @Route("/insertarBD", name="insertarBD")
+     */
+    public function insertar(Request $request) {
+ 
+        /* El objeto debería llamarse Post pero
+         * al ser generado a partir de una base de datos
+         * el objeto se llama como la tabla a la
+         * que representa.
+         */
+ 
+        $post = new Posts();
+ 
+     //   $post->setId($id);
+        $post->setNombre($request);
+        $post->setCantBuscado(1);
+ 
+        //Entity Manager
+        $em = $this->getDoctrine()->getEntityManager();
+ 
+        //Persistimos en el objeto
+        $em->persist($post);
+ 
+        //Insertarmos en la base de datos
+        $flush = $em->flush();
+ 
+        if ($flush == null) {
+            echo "Post creado correctamente";
+        } else {
+            echo "El post no se ha creado";
+        }
+ 
+      
+        die();
+    }
+
+
+
+
+
+
+
 }
